b082ae16ad66c30417ebf5b36825f48e0a171396
juangruoficial <cruz9606@gmail.com>
2023-08-24 14:20:58 -0500

    Using Spread Operator for Reduce props

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import ModalForm from "./components/ModalForm";
-import Err404PopUp from "./components/PopUps/Err404PopUp";
 import RequestSuccesfull from "./components/PopUps/RequestSuccesfull";
 import UserList from "./components/UserList";
 import { useUserManagement } from "./Hooks/useUserManagment";
 
 function App() {
-  const {
-    isShowingModal,
-    isUpdatingUser,
-    users,
-    createUser,
-    deleteUser,
-    handleClickUpdateUser,
-    updateUser,
-    handleToggleModal,
-    urlicon,
-    isShowingPopUp,
-    messagePopUp,
-    isLoginUser,
-    signInUser,
-    isLogged,
-    userLogged,
-  } = useUserManagement();
+  const userManagementProps = useUserManagement();
 
   return (
     <div className="bg-gray-900 min-h-screen">
       <section className="flex">
-        <Header
-          handleToggleModal={handleToggleModal}
-          isLogged={isLogged}
-          userLogged={userLogged}
-        />
-        <UserList
-          users={users}
-          deleteUser={deleteUser}
-          handleClickUpdateUser={handleClickUpdateUser}
-        />
+        <Header {...userManagementProps} />
+        <UserList {...userManagementProps} />
 
-        <ModalForm
-          isShowingModal={isShowingModal}
-          isUpdatingUser={isUpdatingUser}
-          handleToggleModal={handleToggleModal}
-          createUser={createUser}
-          updateUser={updateUser}
-          isLoginUser={isLoginUser}
-          signInUser={signInUser}
-        />
-        <RequestSuccesfull
-          urlicon={urlicon}
-          messagePopUp={messagePopUp}
-          isShowingPopUp={isShowingPopUp}
-        />
+        <ModalForm {...userManagementProps} />
+        <RequestSuccesfull {...userManagementProps} />
       </section>
     </div>
   );
